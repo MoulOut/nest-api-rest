d3fa6fa8d931bf3032725f6ca5292069e14f2b18
MoulOut <mmoulinlima@outlook.com>
2024-02-05 22:35:29 -0300

    feat: add: usuario now has uuid

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,14 +1,22 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsuarioRepository } from './usuario.repository';
 import { criaUsuarioDto } from './dto/CriaUsuario.dto';
+import { UsuarioEntity } from './usuario.entity';
+import { v4 as uuid } from 'uuid';
 
 @Controller('/usuarios')
 export class UsuarioController {
   constructor(private usuarioRepository: UsuarioRepository) {}
   @Post()
   async criaUsuario(@Body() dadosDoUsuario: criaUsuarioDto) {
-    this.usuarioRepository.salvar(dadosDoUsuario);
-    return { message: dadosDoUsuario };
+    const usuarioEntity = new UsuarioEntity();
+    usuarioEntity.email = dadosDoUsuario.email;
+    usuarioEntity.senha = dadosDoUsuario.senha;
+    usuarioEntity.nome = dadosDoUsuario.nome;
+    usuarioEntity.id = uuid();
+
+    this.usuarioRepository.salvar(usuarioEntity);
+    return { id: usuarioEntity.id, message: 'Usuario criado com sucesso.' };
   }
 
   @Get()
