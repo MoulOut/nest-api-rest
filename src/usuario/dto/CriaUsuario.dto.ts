3253e731a19306483f4e004345781c22eae38664
MoulOut <mmoulinlima@outlook.com>
2024-02-05 22:21:10 -0300

    feat: add: new email validation

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -1,9 +1,11 @@
 import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { EmailUnico } from '../validacao/emailUnico.validator';
 
 export class criaUsuarioDto {
   @IsNotEmpty({ message: 'O nome não pode ser vazio.' })
-  name: string;
+  nome: string;
   @IsEmail(undefined, { message: 'O Email enviado é invalido.' })
+  @EmailUnico({ message: 'Já existe um usuario com este e-mail.' })
   email: string;
   @MinLength(8, { message: 'A senha precisa ter no minimo 8 caracteres.' })
   senha: string;
