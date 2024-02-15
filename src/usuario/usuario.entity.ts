e4910856d29b5074318efb8d5d17f2c4c10cdd1e
MoulOut <mmoulinlima@outlook.com>
2024-02-15 09:47:21 -0300

    feat: add: update usuarios entity for new db

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,6 +1,32 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'usuarios' })
 export class UsuarioEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'nome', length: 150, nullable: false })
   nome: string;
+
+  @Column({ name: 'email', length: 70, nullable: false, unique: true })
   email: string;
+
+  @Column({ name: 'senha', length: 255, nullable: false })
   senha: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  created_at: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updated_at: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deleted_at: string;
 }
