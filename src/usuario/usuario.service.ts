5c8d0efb3bf7979087c64c67ec3cc220894b5cf7
MoulOut <mmoulinlima@outlook.com>
2024-02-15 11:05:54 -0300

    feat: add: create usuario service

---
+++ src/usuario/usuario.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ListaUsuarioDto } from './dto/listaUsuario.dto';
+import { UsuarioEntity } from './usuario.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsuarioService {
+  constructor(
+    @InjectRepository(UsuarioEntity)
+    private readonly usuarioRepository: Repository<UsuarioEntity>,
+  ) {}
+  async listaUsuario() {
+    const usuariosSalvos = await this.usuarioRepository.find();
+    const listaUsuarios = usuariosSalvos.map(
+      (usuario) => new ListaUsuarioDto(usuario.id, usuario.nome),
+    );
+
+    return listaUsuarios;
+  }
+}
